6fd5fffadba166abf3d751d550caee88e7e730f1
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-09-22 22:54:20 +0300

    Admin Panel photo gallery

--- new/admin.py
+++ new/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from new.models import Category, New
+from new.models import Category, New, Images
+
+
+class NewImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -10,9 +15,16 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 class NewAdmin(admin.ModelAdmin):
-    list_display = ['category', 'title', 'status', 'image']
+    list_display = ['category', 'title', 'image_tag', 'status']
     list_filter = ['status', 'category']
+    inlines = [NewImageInline]
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['new', 'title', 'image_tag']
+    list_filter = ['new']
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(New, NewAdmin)
+admin.site.register(Images, ImagesAdmin)
--- new/models.py
+++ new/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
     STATUS = (
         ('True', 'Evet'),
@@ -27,7 +30,7 @@ class New(models.Model):
         ('False', 'Hayır'),
     )
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     source = models.CharField(max_length=255)
@@ -39,3 +42,22 @@ class New(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+
+class Images(models.Model):
+    new = models.ForeignKey(New, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+
