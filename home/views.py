ff018089949a5096d1093810288c8c02ff2eaa16
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-09-25 23:46:08 +0300

    References, Contact, About Us pages

--- home/views.py
+++ home/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
 from home.models import Setting
 
@@ -10,3 +9,19 @@ def index(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting': setting}
     return render(request, 'index.html', context)
+
+
+def aboutus(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page': 'aboutus'}
+    return render(request, 'aboutus.html', context)
+
+def contactus(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page': 'contactus'}
+    return render(request, 'contactus.html', context)
+
+def references(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page': 'references'}
+    return render(request, 'references.html', context)
