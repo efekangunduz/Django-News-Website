9a24c96652051a127f730cd533b2f4dd83ed6f57
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-09-26 00:06:54 +0300

    Logo added

--- home/models.py
+++ home/models.py
@@ -21,6 +21,7 @@ class Setting(models.Model):
     smtppassword = models.CharField(blank=True, max_length=10)
     smtpport = models.CharField(blank=True, max_length=5)
     icon = models.ImageField(blank=True, upload_to='images/')
+    logo = models.ImageField(blank=True, upload_to='images/')
     facebook = models.CharField(blank=True, max_length=50)
     instagram = models.CharField(blank=True, max_length=50)
     twitter = models.CharField(blank=True, max_length=50)
--- home/views.py
+++ home/views.py
@@ -16,10 +16,12 @@ def aboutus(request):
     context = {'setting': setting, 'page': 'aboutus'}
     return render(request, 'aboutus.html', context)
 
+
 def contactus(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting': setting, 'page': 'contactus'}
     return render(request, 'contactus.html', context)
+
 
 def references(request):
     setting = Setting.objects.get(pk=1)
