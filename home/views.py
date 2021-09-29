273147dbd06bc4a253fbb13597aacd486b75c3f9
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-09-29 13:12:30 +0300

    ContactFormMessage activated

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactForm, ContactFormMessage
 
 
 def index(request):
@@ -18,8 +19,22 @@ def aboutus(request):
 
 
 def contactus(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, 'Mesajınız başarı ile gönderilmiştir. Teşekkür ederiz.')
+            return HttpResponseRedirect('/contactus')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting, 'page': 'contactus'}
+    form = ContactForm()
+    context = {'setting': setting, 'form': form}
     return render(request, 'contactus.html', context)
 
 
@@ -27,3 +42,4 @@ def references(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting': setting, 'page': 'references'}
     return render(request, 'references.html', context)
+
